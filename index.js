0a156a9540878427552d597e0a046d334ca50a32
melkilic <melike180400@gmail.com>
2020-04-29 15:59:35 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+function findMatching(drivers,string){
+    return drivers.filter(d=> 
+        d.toLowerCase()===string.toLowerCase()
+    )
+}
+
+
+function fuzzyMatch(drivers,string){
+    return drivers.filter(d=> d.charAt(0)=== string.charAt(0)
+    )
+}
+
+function matchName(drivers,string){
+    for(let i=0;i<drivers.length;i++){
+return drivers.filter(d=> d.name===string)
+    }
+    
+}
